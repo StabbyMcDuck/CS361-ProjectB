ae3cb6236bfaefbcb47a0ac028a7a7555ac6768c
StabbyMcDuck <laulessr@gmail.com>
2015-11-25 21:35:43 +0000

    Added database info and password compat info

---
+++ configuration.php
@@ -0,0 +1,8 @@
+<?php
+							
+	$database_configuration = array(
+  	'servername' => 'oniddb.cws.oregonstate.edu',
+    'username' => 'zengx-db',
+    'password' => 'qWXPWG1wbhOLVCAG',
+    'database' => 'zengx-db'
+);
