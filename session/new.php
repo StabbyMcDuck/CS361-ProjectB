e2d91227411a8e664520023228992adc75caaebb
StabbyMcDuck <laulessr@gmail.com>
2015-11-27 17:11:44 +0000

    started shopping list

--- session/new.php
+++ session/new.php
@@ -26,7 +26,7 @@
                     $('#form-errors').html(xhr.responseJSON.message);
                 },
                 success: function(responseJSON, statusText, xhr, formElement) {
-                    $(location).attr('href','<?php echo $root_url ?>index.php');
+                    $(location).attr('href','<?php echo $root_url ?>new-shopping-list.html');
                 }
             };
             $('#form').ajaxForm(options);
