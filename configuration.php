a1bcb589213dc93736f509a7b6f686aceb87cc6c
StabbyMcDuck <laulessr@gmail.com>
2015-11-27 22:35:55 +0000

    data entry problem

--- configuration.php
+++ configuration.php
@@ -3,8 +3,8 @@
 	$database_configuration = array(
   	'servername' => 'oniddb.cws.oregonstate.edu',
     'username' => 'zengx-db',
-    'password' => 'zengx-db',
-    'database' => 'qWXPWG1wbhOLVCAG'
+    'password' => 'qWXPWG1wbhOLVCAG',
+    'database' => 'zengx-db'
 );
 
   $root_url = 'http://web.engr.oregonstate.edu/~imhoffr/CS361-ProjectB-master/'
