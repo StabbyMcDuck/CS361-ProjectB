c20f01baa4307ac5c92f989c2baafb094544cb86
StabbyMcDuck <laulessr@gmail.com>
2015-11-28 03:31:47 +0000

    Fix session logout links

--- shopping-list/new.php
+++ shopping-list/new.php
@@ -78,8 +78,8 @@ if ($mysqli->connect_errno) {
             <div class="header clearfix">
                 <nav>
                     <ul class="nav nav-pills pull-right">
-                        <a href="<?php $root_url ?>index.php">Home</a>
-                        <a href="<?php $root_url ?>session/destroy.php">Log Out</a>
+                        <a href="<?= $root_url ?>index.php">Home</a>
+                        <a href="<?= $root_url ?>session/destroy.php">Log Out</a>
                     </ul>
                 </nav>
                 
