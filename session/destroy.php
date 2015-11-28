db89c1d7dcdc1c6b2a41f6b6e6baf022b9fee352
StabbyMcDuck <laulessr@gmail.com>
2015-11-28 17:39:28 +0000

    Require configuration.php for $root_url

--- session/destroy.php
+++ session/destroy.php
@@ -1,10 +1,15 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 'ON');
+
 session_start();
 
 session_unset();
 
 session_destroy();
+
+require_once('../configuration.php');
 
 header("Location: $root_url");
 
