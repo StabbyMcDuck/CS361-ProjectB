ffa6ebd70aace77f5bbc814231a02d9d418cafa4
StabbyMcDuck <laulessr@gmail.com>
2015-11-28 21:36:55 +0000

    breaks in wrong line! :'(

--- session/new.php
+++ session/new.php
@@ -49,9 +49,9 @@
         <label for="inputEmail" class="sr-only">Email address</label>
         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus name="email">
         <label for="inputPassword" class="sr-only">Password</label>
-        <br>
-        <br>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required name="password">
+        <br>
+        <br>
         <button class="btn btn-lg btn-primary" type="submit">Sign in</button>
         <p id="form-errors">
 
