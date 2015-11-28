3775348d937d3c79665cb9b5e75b1753b206257c
StabbyMcDuck <laulessr@gmail.com>
2015-11-28 21:32:41 +0000

    fixed header

--- session/new.php
+++ session/new.php
@@ -50,7 +50,7 @@
         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus name="email">
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required name="password">
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <button class="btn btn-lg btn-primary" type="submit">Sign in</button>
         <p id="form-errors">
 
         </p>
