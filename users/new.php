288afd98bff0ca350aff9d1dbdcb957abafb823e
StabbyMcDuck <laulessr@gmail.com>
2015-12-02 15:28:51 +0000

    Added wrong username/password

--- users/new.php
+++ users/new.php
@@ -74,6 +74,24 @@
 
     </script>
 
+<script type="text/javascript">
+$(document).on('click', '.formsubmitbutton', function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: $(".form").attr('action'),
+        data: $(".form").serialize(),
+        success: function(response) {
+             if (response === "success") {
+                  window.reload;
+             } else {
+                   alert("invalid username/password.  Please try again");
+             }
+        }
+    });
+});
+</script>
+
     <div class="container">
       <?php
         include("../header.php");
